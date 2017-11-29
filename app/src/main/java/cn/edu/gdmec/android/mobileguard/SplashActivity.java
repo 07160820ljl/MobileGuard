aca15428fb377ef661194f8f0f67bc4ce8434433
07160820ljl <1114316513@qq.com>
2017-11-29 21:25:38 +0800

    手机杀毒版本更新功能。

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,30 +1,42 @@
 package cn.edu.gdmec.android.mobileguard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import cn.edu.gdmec.android.mobileguard.m1home.utils.MyUtils;
-import cn.edu.gdmec.android.mobileguard.m1home.utils.VersionUpdateUtils;
+import java.io.File;
 
 public class SplashActivity extends AppCompatActivity {
     private TextView mTvVersion;
     private String mVersion;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+      /*  requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
         mVersion = MyUtils.getVersion(getApplicationContext());
-        mTvVersion = (TextView) findViewById(R.id.tv_splash_version);
-        mTvVersion.setText("版本号：" + mVersion);
-        final VersionUpdateUtils versionUpdateUtils = new VersionUpdateUtils(mVersion, SplashActivity.this);
-        new Thread() {
+        mTvVersion=(TextView)findViewById(R.id.tv_splash_version);
+        mTvVersion.setText("版本号"+mVersion);
+        final VersionUpdateUtils versionUpdateUtils=new VersionUpdateUtils(mVersion,SplashActivity.this);
+        new Thread(){
             @Override
-            public void run() {
+            public void run(){
                 super.run();
                 versionUpdateUtils.getCloudVersion();
             }
-        }.start();
+        }.start();*/
+
+    }
+    public static void installApk(Activity activity,String apkFile){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setDataAndType(Uri.fromFile(
+                new File(Environment.getExternalStoragePublicDirectory("/download/").getPath()+"/"+apkFile)
+        ),"application/vnd.android.package-archive");
+        activity.startActivityForResult(intent,0);
     }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/dao/AntiVirusDao.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/dao/AntiVirusDao.java
@@ -5,25 +5,40 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 /**
- * Created by ASUS on 2017/11/18.
+ * Created by student on 17/10/17.
  */
 
 public class AntiVirusDao {
+
     private static Context context;
     private static String dbname;
     public AntiVirusDao(Context context){
         this.context = context;
-        dbname = "/data/data/"+context.getPackageName()+"/files/antivirus.db";
+        dbname = "/data/data/"+context.getPackageName ()+"/files/antivirus.db";
     }
-    public  String checkVirus(String md5){
+    public String checkVirus(String md5){
         String desc = null;
+        SQLiteDatabase db = SQLiteDatabase.openDatabase (
+                dbname, null,
+                SQLiteDatabase.OPEN_READONLY );
+        Cursor cursor = db.rawQuery ( "select desc from datable where md5=?",
+                new String[] { md5 });
+        if (cursor.moveToNext ()){
+            desc = cursor.getString ( 0 );
+        }
+        cursor.close ();
+        db.close ();
+        return desc;
+    }
+    public String getVirusVersion(){
+        String desc = null;
+
         SQLiteDatabase db = SQLiteDatabase.openDatabase(
-                dbname,null,
+                dbname, null,
                 SQLiteDatabase.OPEN_READONLY);
-        Cursor cursor = db.rawQuery("select desc from datable where md5=?",
-                new String[]{md5});
+        Cursor cursor = db.rawQuery("select * from version",null);
         if (cursor.moveToNext()){
-            desc = cursor.getString(0);
+            desc = cursor.getString(0)+"."+cursor.getString(1)+"."+cursor.getString(2);
         }
         cursor.close();
         db.close();
