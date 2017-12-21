97ee638a3899e503dd1a8828c6201dfffebb1a5a
07160820ljl <1114316513@qq.com>
2017-12-21 15:59:45 +0800

    密码锁

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,10 +1,14 @@
 package cn.edu.gdmec.android.mobileguard;
 
 import android.app.Activity;
+import android.app.AppOpsManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -13,9 +17,15 @@ import java.io.File;
 public class SplashActivity extends AppCompatActivity {
     private TextView mTvVersion;
     private String mVersion;
+    private static final int MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS = 1101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!hasPermission()){
+            startActivityForResult(
+                    new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS),
+                    MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS);
+        }
       /*  requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
         mVersion = MyUtils.getVersion(getApplicationContext());
@@ -38,5 +48,25 @@ public class SplashActivity extends AppCompatActivity {
                 new File(Environment.getExternalStoragePublicDirectory("/download/").getPath()+"/"+apkFile)
         ),"application/vnd.android.package-archive");
         activity.startActivityForResult(intent,0);
+    }
+    private boolean hasPermission(){
+        AppOpsManager appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+        int mode = 0;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT){
+            mode =appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                    android.os.Process.myUid(),getPackageName());
+        }
+        return mode == AppOpsManager.MODE_ALLOWED;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS){
+            if (!hasPermission()){
+                startActivityForResult(
+                        new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS),
+                        MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS);
+            }
+        }
     }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/BootCompleteReceiver.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/BootCompleteReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 
 import cn.edu.gdmec.android.mobileguard.App;
+import cn.edu.gdmec.android.mobileguard.m9advancedtools.service.AppLockService;
 
 /**
  * Created by ASUS on 2017/10/21.
@@ -14,5 +15,6 @@ public class BootCompleteReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
         ((App)(context.getApplicationContext())).correctSIM();
+        context.startActivity(new Intent(context, AppLockService.class));
     }
 }
